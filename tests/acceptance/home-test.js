38adbad9162d40590006b660679c610c10ee62b7
João Guilherme Farias Duda <jgfd@cin.ufpe.br>
2017-07-28 12:44:42 -0300

    Home page tests

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -3,10 +3,30 @@ import moduleForAcceptance from '../helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | home');
 
-test('visiting /login', function(assert) {
+test('visiting home page', function(assert) {
   visit('/');
 
   andThen(function() {
     assert.equal(currentURL(), '/');
+    assert.ok($('.table-packages .package-link').is( ":visible" ), "the addon is visible");
+  });
+});
+
+test('checking search field placeholder', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal($('.search-field').attr('placeholder'), 'Search 3943 addons');
+  });
+});
+
+
+test('visiting query page directly', function(assert) {
+  visit('/?query=awesome-macros');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/?query=awesome-macros');
+    var addon = $('.table-packages .package-link');
+    assert.equal(addon.first().text(), 'ember-awesome-macros');
+    assert.equal(addon.length, 1);
   });
 });
